6744b2a0674ea71e4c466d4ca83e72bf1fdcb47c
Alves <josealvesfn@gmail.com>
2024-03-02 23:37:05 -0300

    mais uma mudanca

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
         System.out.println("Hello World!");
-        
+        System.out.println("Continue");
     }
 }
